118a9a8cbae116aa84b04af2939f4367c4fd2343
Omar Elkholy <omaralkholy2021@gmail.com>
2024-06-18 09:07:27 +0300

    Add sidebar UI

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../shared/components/header/header.component";
-import { SidebarComponent } from "../../shared/components/sidebar/sidebar.component";
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
-    selector: 'app-main-layout',
-    standalone: true,
-    templateUrl: './main-layout.component.html',
-    styleUrl: './main-layout.component.scss',
-    imports: [HeaderComponent, SidebarComponent, RouterOutlet]
+  selector: 'app-main-layout',
+  standalone: true,
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.scss',
+  imports: [HeaderComponent, SidebarComponent, RouterOutlet, MatSidenavModule],
 })
-export class MainLayoutComponent {
-
-}
+export class MainLayoutComponent {}
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatButtonModule, MatIconModule, MatCardModule, MatDatepickerModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-
+  selected = model<Date | null>(null);
 }
