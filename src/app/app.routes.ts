5a25c57b9bb02dd007e668e75791f0f2922a636c
Omar Elkholy <omaralkholy2021@gmail.com>
2024-06-18 07:32:31 +0300

    Add project structure and files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layout/main-layout/main-layout.component').then(
+        (m) => m.MainLayoutComponent
+      ),
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./feature/home/home.component').then((m) => m.HomeComponent),
+      },
+    ],
+  },
+];
---
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../shared/components/header/header.component";
+import { SidebarComponent } from "../../shared/components/sidebar/sidebar.component";
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'app-main-layout',
+    standalone: true,
+    templateUrl: './main-layout.component.html',
+    styleUrl: './main-layout.component.scss',
+    imports: [HeaderComponent, SidebarComponent, RouterOutlet]
+})
+export class MainLayoutComponent {
+
+}
