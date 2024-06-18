47a70cd260c3c73b4a80a7aea6e573543d0357e0
Omar Elkholy <omaralkholy2021@gmail.com>
2024-06-18 08:25:33 +0300

    Add header UI

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { DatePipe } from '@angular/common';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    DatePipe,
+    MatMenuModule,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  date = new Date();
 
+  setTodaysDate() {
+    this.date = new Date();
+  }
+  setPrevDate() {
+    const newDate = new Date(this.date);
+    newDate.setDate(newDate.getDate() - 1);
+    this.date = newDate;
+  }
+
+  setNextDate() {
+    const newDate = new Date(this.date);
+    newDate.setDate(newDate.getDate() + 1);
+    this.date = newDate;
+  }
 }
